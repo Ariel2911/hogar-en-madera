70c4a7c3d7da457163725e52a62056b1c188f2d9
Ariel Barrios <ariel29111978@hotmail.com>
2023-04-24 20:08:07 -0300

    Agrega el componente Footer y los archivos necesarios para su renderización

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,47 @@ import Button, { StyleTypeEnum } from './components/button';
 import Tag, { TagTypeEnum } from './components/tag';
 import ArrowButton, { ArrowTypeEnum } from './components/arrowButton';
 import Category, { CategoryTypeEnum } from './components/category';
-import { iconBag } from './components/iconos';
 // import Thumbnails from './components/thumbnails';
 import ProductCard, { ProductCardTypeEnum } from './components/productCard';
 import Header from './components/header';
+import Footer from './components/footer';
+
+import {
+	iconBag,
+	iconPhone,
+	iconEmail,
+	iconAddres,
+	facebook,
+	instagram,
+	twitter,
+} from './components/iconos';
 
 import imgCategory from './assets/categoty/organizadores.png';
 import imgProduct00 from './assets/product/lampara farol00.png';
 import imgProduct01 from './assets/product/lampara farol01.png';
 import imgProduct02 from './assets/product/lampara farol02.png';
 import logo from './assets/header/Logo.png';
+import logoFooter from './assets/footer/logoFooter.png';
 
 function App(): JSX.Element {
 	const navBarItems = ['inicio', 'categorias', 'productos', 'contacto'];
+
+	const contactInformation = [
+		{
+			icon: iconPhone,
+			data: '1155555555',
+		},
+		{
+			icon: iconEmail,
+			data: 'ariel29111978@hotmail.com',
+		},
+		{
+			icon: iconAddres,
+			data: 'Av. Corrientes 2333 2do “A” Balvanera',
+		},
+	];
+
+	const socialNetwork = [facebook, instagram, twitter];
 
 	return (
 		<ThemeProvider theme={theme}>
@@ -93,7 +121,7 @@ function App(): JSX.Element {
 					categoty='Lámparas'
 					price='$399.00'
 				/>
-
+				{/* 
 				<br />
 				<ProductCard
 					title='Lámpara Farol'
@@ -110,13 +138,20 @@ function App(): JSX.Element {
 					productCardType={ProductCardTypeEnum.card}
 					categoty='Lámparas'
 					price='$399.00'
-				/>
+				/> */}
 			</div>
 			<br />
 			<br />
 			<hr />
 			<br />
 			<br />
+			<Footer
+				logo={logoFooter}
+				socialNetwork={socialNetwork}
+				contactInformation={contactInformation}
+				copyRight={'2023 Marca'}
+				madeIn={'Ariel Barrios'}
+			/>
 		</ThemeProvider>
 	);
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,90 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+	padding: 1rem 1rem 0.25rem 1rem;
+	background-color: ${({ theme }) => theme.colors.primary};
+	color: ${({ theme }) => theme.colors.typography.secondaryTypography};
+	& h2 {
+		font-size: 1rem;
+		margin-bottom: 0.5rem;
+	}
+	& p {
+		margin-bottom: 0;
+		font-size: 0.75rem;
+	}
+	& a {
+		padding: 0;
+	}
+	& svg path {
+		fill: white;
+	}
+`;
+
+const Logo = styled.img`
+	width: 100px;
+	height: 24px;
+`;
+
+const SocialNetwork = styled.div`
+	display: flex;
+	gap: 0.5rem;
+`;
+
+const Contact = styled.div``;
+
+const ContactInformation = styled.div`
+	display: flex;
+	align-items: center;
+	gap: 0.5rem;
+	margin: 0.25rem 0;
+	& p {
+		margin: 0;
+	}
+`;
+
+const Footer = styled.div`
+	text-align: center;
+	& p {
+		margin: 0;
+	}
+`;
+
+interface Props {
+	logo: string;
+	socialNetwork: JSX.Element[];
+	contactInformation: Array<{
+		icon: JSX.Element;
+		data: string;
+	}>;
+	copyRight: string;
+	madeIn: string;
+}
+
+const App = (props: Props): JSX.Element => (
+	<Container>
+		<Logo src={props.logo} />
+		<h2>Seguinos</h2>
+		<SocialNetwork>
+			{props.socialNetwork.map((item, id) => (
+				<a key={id} href=''>
+					{item}
+				</a>
+			))}
+		</SocialNetwork>
+		<Contact>
+			<h2>Contactanos</h2>
+			{props.contactInformation.map((item, id) => (
+				<ContactInformation key={id}>
+					{item.icon}
+					<p>{item.data}</p>
+				</ContactInformation>
+			))}
+		</Contact>
+		<Footer>
+			<p>Copyright © {props.copyRight}</p>
+			<p>Desarrollado por {props.madeIn}</p>
+		</Footer>
+	</Container>
+);
+
+export default App;
